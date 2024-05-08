500b5723c15a650c8536937a1365b79e34345cc5
Gaurav Sharma <gaurav219688@gmail.com>
2024-05-09 01:26:53 +0530

    created rag

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from langchain_openai import ChatOpenAI # openai chat models
+from langchain_openai import ChatOpenAI, OpenAIEmbeddings # openai chat models
 from langchain_core.prompts import ChatPromptTemplate # to create a chat qa_prompt
 from langchain_community.llms import Ollama, HuggingFaceEndpoint # to use the llama3 and gemma models
 from langchain_groq import ChatGroq
@@ -11,12 +11,21 @@ from src.utils import Utils, agents
 from fastapi import FastAPI
 import uvicorn
 from langserve import add_routes
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores.chroma import Chroma
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains.retrieval import create_retrieval_chain
+from langchain_core.messages import HumanMessage, SystemMessage, AIMessage
 
 load_dotenv()
 
 # setting up langsmith tracing
 os.environ["LANGCHAIN_TRACING_V2"] = "true" # enable tracking in langsmith, the results can be seen on the dashboard
 os.environ["LANGCHAIN_PROJECT"] = "multiutility-llm-app" # project name for tracking
+
+# creating the artifacts folder
+os.makedirs("artifacts", exist_ok=True)
 
 # creating a qa_prompt
 qa_prompt = ChatPromptTemplate.from_messages(
@@ -32,7 +41,7 @@ output_parser = StrOutputParser() # initialize the output parser
 st.title("ChatBot")
 
 with st.sidebar:
-    service = option_menu("Select Service", ["Home","Multi LLM QnA","Chat With Website"], default_index=0)    
+    service = option_menu("Select Service", ["Home","Multi LLM QnA","Chat With Website","RAG"], default_index=0)    
 
 # >----------------- Home -----------------< #
 if service == "Home":
@@ -76,3 +85,34 @@ elif service == "Chat With Website":
         executor = agents.get_executor(selected_website)
         if (input_text) and input_text!="":
             st.write(executor.invoke({"input":input_text})['output'])
+
+# >----------------- RAG -----------------< #
+elif service == "RAG":
+    st.subheader("Chat with your files")
+    uploaded_files = st.file_uploader(label="Upload a PDF file(s)", type=["pdf"], accept_multiple_files=True)
+    os.makedirs("artifacts/uploaded_files", exist_ok=True)
+    if uploaded_files:
+        text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+        all_documents = []
+        for uploaded_file in uploaded_files:
+            file_path = f"artifacts/uploaded_files/{uploaded_file.name}"
+            with open(file_path,"wb") as f:
+                f.write(uploaded_file.getbuffer())
+            docs = PyPDFLoader(file_path).load()
+            documents = text_splitter.split_documents(docs)
+            all_documents.extend(documents)
+        db = Chroma.from_documents(all_documents, embedding=OpenAIEmbeddings())
+        retriever = db.as_retriever()
+        llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0.5)
+        prompt = ChatPromptTemplate.from_messages([
+            SystemMessage(content = "Answer the following question based only on the provided context."),
+            SystemMessage(content = "Think step by step before providing a detailed answer."),
+            ("system","Context:\n{context}"),
+            ("user","Question:\n{input}"),
+        ])
+        document_chain = create_stuff_documents_chain(llm=llm, prompt=prompt)
+        retrieval_chain = create_retrieval_chain(retriever, document_chain)
+    input_text = st.text_input("Enter your question")
+    if (input_text) and input_text!="":
+        response = retrieval_chain.invoke({'input':input_text})
+        st.write(response['answer'])
--- src/utils/Utils.py
+++ src/utils/Utils.py
@@ -1,7 +1,10 @@
 import streamlit as st
 from langchain_openai import ChatOpenAI
+from langchain_community.chat_models.ollama import ChatOllama
+from langchain_community.chat_models.huggingface import ChatHuggingFace
 from langchain_community.llms import Ollama, HuggingFaceEndpoint
 from langchain_groq import ChatGroq
+import os
 
 
 # >----------------- Multi LLM QnA -----------------< #
@@ -44,11 +47,12 @@ def initialize_qa_llm(selected_llm):
     if selected_llm == "OpenAI":
         llm = ChatOpenAI(model="gpt-3.5-turbo")
     elif selected_llm == "Ollama":
-        llm = Ollama(model="llama3:instruct")
-    elif selected_llm == "Groq API":
+        llm = ChatOllama(model="llama3:instruct")
+    elif selected_llm == "Groq":
         llm = ChatGroq(model="gemma-7b-it", api_key=os.getenv("GROQ_API_KEY"))
-    elif selected_llm == "Hugging Face API":
-        llm = HuggingFaceEndpoint(repo_id="mistralai/Mistral-7B-Instruct-v0.2", max_length=128, temperature=0.5)
+    elif selected_llm == "HuggingFace":
+        base_llm = HuggingFaceEndpoint(repo_id="mistralai/Mistral-7B-Instruct-v0.2", max_length=128, temperature=0.5)
+        llm = ChatHuggingFace(llm=base_llm)
     return llm
 
 # >----------------- Chat With Website -----------------< #
